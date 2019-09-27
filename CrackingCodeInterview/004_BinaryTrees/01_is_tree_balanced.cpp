70c9d74b29433d40655b1f273bab620794eec668
gw <kuvykin@lsrm.ru>
2019-09-27 14:41:15 +0300

    BinaryTrees: is balanced

--- CrackingCodeInterview/004_BinaryTrees/01_is_tree_balanced.cpp
+++ CrackingCodeInterview/004_BinaryTrees/01_is_tree_balanced.cpp
@@ -5,5 +5,36 @@ from the root by more than one.
 
 Algo:
 is_balanced(root):
+    lb, lh = is_balanced(root->left)
+    if (!lb) return {false, 0}
+    rb, rh = is_balanced(root->left)
+    if (!rb || abs(lh - rh) > 1):
+        return {false, 0}
+    return {true, max(lh, rh)+1}
     
+Algo2:
+|maxDepth - minDepth| <= 1
 */
+
+struct Node {
+    int value = 0;
+    Node* parent = nullptr;
+    Node* left = nullptr;
+    Node* right = nullptr;    
+};
+
+#include <optional>
+
+using namespace std;
+
+optional<int> is_balanced(Node* root) {
+    if (!root) return nullopt;
+    auto lhs = is_balanced(root->left);
+    if (!lhs) return nullopt;
+    auto rhs = is_balanced(root->right);
+    if (!rhs || abs(*lhs - *rhs) > 1) return nullopt;
+    return max(*lhs, *rhs)+1;
+}
+
+
+
