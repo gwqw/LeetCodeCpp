90382fed0ee5da3afae88f3f07ce916a3c836856
gw <kuvykin@lsrm.ru>
2019-10-01 17:54:07 +0300

    changes in graph algo

--- CrackingCodeInterview/004_BinaryTrees/00_graph.cpp
+++ CrackingCodeInterview/004_BinaryTrees/00_graph.cpp
@@ -1,4 +1,5 @@
 /**
+Graph: n=|V|, m=|E|
 Depth First Search: DFS involves searching a node and all its children before proceed-
 ing to its siblings.
 Breadth First Search: BFS involves searching a node and its siblings before going on
@@ -7,53 +8,66 @@ to any children.
 
 #include <vector>
 #include <unordered_set>
-#include <deque>
+#include <queue>
+#include <stack>
 
 using Vertex = int;
-
-struct Node {
-    Vertex v = 0;
-    std::vector<Vertex> links;
-};
-
+using Node = std::vector<Vertex>;
 using Graph = std::vector<Node>;
 
 using namespace std;
 
-Node* dfs(const Graph& graph, Vertex from, Vertex to, unordered_set<Vertex>& visited) {
-    if (graph.empty()) return nullptr;
-    
-    if (from == to) return &graph[from];
-    
+///recursive dfs 
+void dfs(const Graph& graph, Vertex from, unordered_set<Vertex>& visited) {
+    if (graph.empty()) return;
+
     visited.insert(from);
-    for (auto v : graph[from].links) {
+    for (auto v : graph[from]) {
         if (visited.count(v)) continue;
         visited.insert(v);
-        auto res = dfs(graph, v, to);
-        if (res) return res;
+        dfs(graph, v, visited);
     }
-    return nullptr;
 }
 
-Node* bfs(const Graph& graph, Vertex from, Vertex to) {
-    is (graph.empty()) return nullptr;
-    
-    if (from == to) return &graph[from];
+/// nonrecursive dfs
+void dfs(const Graph& graph, Vertex from) {
+    if (graph.empty()) return;
+
     unordered_set<Vertex> visited;
-    visited.insert(from);
-    deque<Vertex> cache;
-    cache.push_back(from);
+    stack<Vertex> cache;
+    cache.push(from);
     while (!cache.empty()) {
-        Vertex v = cache.front();
-        cache.pop_front();
+        Vertex v = cache.top();
+        cache.pop();
         if (visited.count(v)) continue;
-        if (v == to) return &graph[v];
         visited.insert(v);
-        
-        for (Vertex c : graph[v].links) {
+
+        for (Vertex c : graph[v]) {
             if (!visited.count(c)) {
-                cache.push_back(c);
+                cache.push(c);
             }
         }
     }
 }
+
+/// nonrecursive bfs
+void bfs(const Graph& graph, Vertex from) {
+    if (graph.empty()) return;
+
+    unordered_set<Vertex> visited;
+    queue<Vertex> cache;
+    cache.push(from);
+    while (!cache.empty()) {
+        Vertex v = cache.front();
+        cache.pop();
+        if (visited.count(v)) continue;
+        visited.insert(v);
+
+        for (Vertex c : graph[v]) {
+            if (!visited.count(c)) {
+                cache.push(c);
+            }
+        }
+    }
+}
+
