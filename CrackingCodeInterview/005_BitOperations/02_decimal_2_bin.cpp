bee5d9e5d81ef00bdbfb5b43346c63a738b01df9
= <=>
2019-10-18 16:08:04 +0300

    CrackingCode: added solutions to 02 and 03 tasks

--- CrackingCodeInterview/005_BitOperations/02_decimal_2_bin.cpp
+++ CrackingCodeInterview/005_BitOperations/02_decimal_2_bin.cpp
@@ -23,9 +23,19 @@ for d:
     
 // dec frac to bin
 1/10 -> 
-a1/2^1 + a2/2^2 + ai/2^i + a/2^n
-*2
-get int() -> a
+a1/2^1 + a2/2^2 + ai/2^i + a/2^n = Sum(ai * (1/2)^i)
+
+a1 = int(frac * 2^1)
+frac -= a1/(2)^1
+a2 = int(frac * 2^2)
+frac -= a2 / (2)^2
+
+mult = 2
+while frac > 1e-10:
+    a = int(frac * mult)
+    frac -= a / mult
+    mult *= 2
+return {a1..an}
 */
 
 std::string int_to_binstr(int num) {
@@ -48,8 +58,26 @@ std::string bin_frac_to_dec(const std::string& bfrac) {
     return to_string(n);
 }
 
-std::string dec_frac_bin()
+std::vector<int> dec_frac_to_bin(double frac) {
+    vector<int> res;
+    mult = 2;
+    while (fabs(frac) > 1e-10) {
+        int a = int(frac * mult);
+        frac -= (double)a / mult;
+        mult *= 2;
+    }
+    return res;
+}
 
-
-
+std::string real_to_bin(std::string num_str) {
+    int num = stol(num_str);
+    string res = int_to_binstr(num) + '.';
+    double frac = stod(num_str) - num;
+    auto frac_digits = dec_to_frac(frac);
+    res.reserve(res.size() + frac_digits.size());
+    for (int d : frac_digits) {
+        res += char(d) + '0';
+    }
+    return res;    
+}
 
---
+++ CrackingCodeInterview/005_BitOperations/03_next_int.cpp
@@ -0,0 +1,72 @@
+/**
+Given an integer, print the next smallest and next largest number that have the 
+same number of 1 bits in their binary representation.
+
+00 -> 00, 00
+10 -> 01, 100
+11 -> 11, 101
+
+Algo1:
+    calc 1 nums
+    if all 1: num is smallest
+    go backward:
+        calc 1 nums if the same -- stop
+    go forward:
+        calc 1 nums if the same -- stop
+
+
+Algo2:
+    0 -> 0, 0
+    largest:
+    find first 0 (from backward) and not the real first bit: 
+    if it is not the last:      10010101>0<_1_111111
+        swap 0 with right 1:    10010101_1_>0<111111
+    else:                               111111      110
+        insert 0 in second position     1011111     1010
+        
+    smallest:
+    find first 1 (from backward) it must not be first digit: 1010101>1<000
+        if bit is not the last:         10>1<_0_0
+            swap it with the right 0:   10_0_>1<0
+        else:   111
+        it is the smallest
+*/
+
+bool check_bit(int num, int pos) {
+  return num & (1 << pos);
+}
+
+void set_bit(int& num, int pos) {
+  num |= (1 << pos);
+}
+
+void unset_bit(int& num, int pos) {
+  num &= !(1 << pos);
+}
+
+void switch_bit(int& num, int pos) {
+  num ^= (1 << pos);
+}
+
+std::pair<int,int> get_next_ints(int num) {
+    if (nums == 0) return make_pair(0,0);
+    
+    int small = num;
+    int large = num;
+    
+    //smallest
+    int one_pos = 0;
+    for (int pos = 0; pos < 31; ++pos) {
+        if (check_bit(num, pos) && pos) {
+            one_pos = pos;
+            break;
+        }
+    }
+    if (one_pos) {
+        unset_bit(small, one_pos);
+        one_pos--;
+        set_bit(small, one_pos);
+    }
+    
+    // largest
+}
