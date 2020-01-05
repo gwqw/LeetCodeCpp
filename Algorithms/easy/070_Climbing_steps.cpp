8c184d1604b88885758705612fd67af2d5ef2a25
gw <gwrass@mail.ru>
2020-01-05 23:42:34 +0300

    Leetcode: 70 corrections

--- Algorithms/easy/070_Climbing_steps.cpp
+++ Algorithms/easy/070_Climbing_steps.cpp
@@ -17,18 +17,40 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 
-Algo:
-l1 = 1
-l2 = 0
-for i in range(1, n):
-  t = l2
-  l2 = l1
-  l1 += t
-return l1 + l2
+1 - 1
+2 - 2
+3 - 3
+4 - 5
 
-O(N), O(1) mem
+Algo: O(1 if int, N if long arithm) + O(1) mem
+next = cur + prev (Fibonacci)
+prev = 1
+cur = 2
+for i in range(3, n):
+  next = prev + cur
+  prev = cur
+  cur = next
+return prev + cur
+
+
 */
 
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 1) return 1;
+        int64_t prev = 1, cur = 1;
+        for (int i = 2; i < n; ++i) {
+            int64_t next = prev + cur;
+            prev = cur;
+            cur = next;
+        }
+        return prev + cur;
+    }
+};
+
+
+// old
 class Solution {
 public:
     int climbStairs(int n) {
