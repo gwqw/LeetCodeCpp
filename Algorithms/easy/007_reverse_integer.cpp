faa58bc54fa3ad520f1a91ab8741152ab3881e68
gw <kuvykin@lsrm.ru>
2020-01-14 14:23:00 +0300

    Add algo to 007 Leetcode

--- Algorithms/easy/007_reverse_integer.cpp
+++ Algorithms/easy/007_reverse_integer.cpp
@@ -10,25 +10,34 @@
     Example 3:
         Input: 120
         Output: 21
+        
+Algo: O(D)
+- save sign -> is_minus
+- take digit: d = n % 10
+- remove digit n /= 10
+- check for overflow
+- add digit to res: res = res*10 + d
+- repeat while n != 0
+- 
 */
 
 class Solution {
 public:
     int reverse(int x) {
-      bool is_minus = false;
-      if (x <= INT32_MIN) {return 0;}
-      if (x < 0) { is_minus = true; x = -x;}
-      int res = 0;
-      while (x > 0) {
-        char digit = x % 10;
-        x /= 10;
-        if (res < INT32_MAX / 10 || (res == INT32_MAX / 10 && digit <= INT32_MAX % 10)) {
-          res = res*10 + digit;
-        } else {
-          return 0;
+        if (x == INT_MIN) return 0;
+        bool is_minus = false;
+        if (x < 0) { is_minus = true; x = -x;}
+        int res = 0;
+        while (x > 0) {
+            char digit = x % 10;
+            x /= 10;
+            if (res < INT32_MAX / 10 || (res == INT32_MAX / 10 && digit <= INT32_MAX % 10)) {
+                res = res*10 + digit;
+            } else {
+                return 0;
+            }
         }
-      }
-      if (is_minus) {res = -res;}
-      return res;
+        if (is_minus) {res = -res;}
+        return res;
     }
 };
