2b17fc5109bddc7bf892d4bd195cc508c2796fdc
gw <kuvykin@lsrm.ru>
2020-03-06 12:12:11 +0300

    Leecode: 094

--- Algorithms/medium/094_binary_tree_in_order_traversal.cpp
+++ Algorithms/medium/094_binary_tree_in_order_traversal.cpp
@@ -12,6 +12,10 @@ Input: [1,null,2,3]
 
 Output: [1,3,2]
 Follow up: Recursive solution is trivial, could you do it iteratively?
+
+Algo1: recursive dfs
+
+Algo2: iterative dfs
 */
 
 /**
@@ -26,6 +30,58 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+    
+private:
+    void inorder(TreeNode* node, vector<int>& nodes) {
+        if (!node) return;
+        inorder(node->left, nodes);
+        nodes.push_back(node->val);
+        inorder(node->right, nodes);
     }
 };
+
+/*
+    1
+     \
+      2
+     /
+    3
+    st: 
+    v:  1, 3, 2
+    r:  1, 3, 2
+    
+*/
+
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (!root) return {};
+        vector<int> res;
+        stack<TreeNode*> st;
+        st.push(root);
+        unordered_set<TreeNode*> visited;
+        while (!st.empty()) {
+            auto node = st.top();
+            if (node->left && visited.count(node->left) == 0) {
+                st.push(node->left);
+            } else {
+                st.pop();
+                res.push_back(node->val);
+                visited.insert(node);
+                if (node->right) {
+                    st.push(node->right);
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+
