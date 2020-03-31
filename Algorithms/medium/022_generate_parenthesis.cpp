a0904ba0f188516ee3106dd81a952f95e0efc088
gw <gwrass@mail.ru>
2020-03-31 19:16:02 +0300

    Leetcode: 022 (edt)

--- Algorithms/medium/022_generate_parenthesis.cpp
+++ Algorithms/medium/022_generate_parenthesis.cpp
@@ -15,6 +15,12 @@ Algo: recursive
 - split every call:
     - call with cur+'(' if '(' count < n
     - call with cur+')' if ')' count < '(' count
+    
+Algo: itterative
+- go from end and calc opened and closed
+- if open and closed > opened:
+    - paste close here
+    - form rest minimum string: opened*'(' + closed-1*')'
 */
 
 class Solution {
@@ -37,6 +43,54 @@ private:
         if (close < open) {
             generateParenthesis(n, open, close+1, brackets + ')', ans);
         }
+    }
+
+};
+
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        string par_str;
+        par_str.reserve(2*n);
+        for (int i = 0; i < n; ++i) {
+            par_str.push_back('(');
+        }
+        for (int i = 0; i < n; ++i) {
+            par_str.push_back(')');
+        }
+        vector<string> ans;
+        do {
+            ans.push_back(par_str);
+        } while (generateNextParenthesis(par_str));
+        return ans;
+    }
+    
+private:
+    bool generateNextParenthesis(string& str) {
+        int opened = 0;
+        int closed = 0;
+        for (int i = (int)str.size()-1; i > 0; --i) {
+            if (str[i] == ')') {
+                ++closed;
+            } else {
+                ++opened;
+                if (closed > opened) {
+                    str[i] = ')';
+                    --closed;
+                    while (opened > 0) {
+                        str[++i] = '(';
+                        --opened;
+                    }
+                    while (closed > 0) {
+                        str[++i] = ')';
+                        --closed;
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 };
